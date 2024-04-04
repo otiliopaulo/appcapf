69d0213d42db27a999e42c8aa10ac5bb13d1aa66
otiliopaulo <otiliopaulo@gmail.com>
2024-04-04 18:48:07 -0300

    Atualização da classe main

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AppCAPF',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
